6d24b3d0f77035fe43854621e3d653c46c7641bc
hndrr <yozhik20@gmail.com>
2021-09-28 21:32:39 +0900

    fix: filepickerを追加

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'dart:io';
 import 'dart:typed_data';
 
+import 'package:audio_query_sample/file_picker.dart';
 import 'package:fab_circular_menu/fab_circular_menu.dart';
 import 'package:flutter/material.dart';
 import 'package:on_audio_query/on_audio_query.dart';
@@ -10,6 +11,7 @@ void main() {
   runApp(Songs());
 }
 
+// ignore: use_key_in_widget_constructors
 class Songs extends StatefulWidget {
   @override
   _SongsState createState() => _SongsState();
@@ -18,11 +20,11 @@ class Songs extends StatefulWidget {
 class _SongsState extends State<Songs> {
   OnAudioQuery audioQuery = OnAudioQuery();
   AudioPlayer audioPlayer = AudioPlayer();
-  final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
   final GlobalKey<FabCircularMenuState> fabKey = GlobalKey();
   bool _isPlaying = false;
   bool _visible = false;
   String filedataPath = '';
+  int _selectedIndex = 0;
 
   @override
   void initState() {
@@ -94,99 +96,37 @@ class _SongsState extends State<Songs> {
     }
   }
 
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
+    final _widgetList = <Widget>[
+      _audioQueryBody(context),
+      FilePickerDemo(),
+    ];
+
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: const Text('OnAudioQueryExample'),
           elevation: 2,
         ),
-        body: FutureBuilder<List<SongModel>>(
-          future: OnAudioQuery().querySongs(
-            sortType: SongSortType.DEFAULT,
-            orderType: OrderType.ASC_OR_SMALLER,
-            uriType: UriType.EXTERNAL,
-          ),
-          builder: (context, item) {
-            // Loading content
-            if (item.data == null) {
-              return const CircularProgressIndicator();
-            }
-
-            // When you try "query"
-            // without asking for [READ] or [Library] permission
-            // the plugin will return a [Empty] list.
-            if (item.data!.isEmpty) {
-              return const Text('Nothing found!');
-            }
-
-            // You can use [item.data!] direct or you can create a:
-            // List<SongModel> songs = item.data!;
-            return ListView.builder(
-              itemCount: item.data!.length,
-              itemBuilder: (context, index) {
-                return ListTile(
-                  title: Text(item.data![index].title),
-                  subtitle: Text(item.data![index].artist ?? 'No Artist'),
-                  // trailing: const Icon(Icons.play_arrow),
-                  // This Widget will query/load image. Just add the id and type.
-                  // You can use/create your own widget/method using [queryArtwork].
-                  leading: FutureBuilder<Uint8List?>(
-                      future: OnAudioQuery().queryArtwork(
-                        item.data![index].id,
-                        ArtworkType.AUDIO,
-                        format: ArtworkFormat.JPEG,
-                        size: 200,
-                      ),
-                      builder: (context, item) {
-                        if (item.data != null && item.data!.isNotEmpty) {
-                          return ClipRRect(
-                            // borderRadius: BorderRadius.circular(50),
-                            clipBehavior: Clip.antiAlias,
-                            child: Image.memory(
-                              item.data!,
-                              gaplessPlayback: false,
-                              repeat: ImageRepeat.noRepeat,
-                              scale: 1,
-                              width: 50,
-                              height: 50,
-                              fit: BoxFit.cover,
-                              // color: artworkColor,
-                              // colorBlendMode: artworkBlendMode,
-                              filterQuality: FilterQuality.low,
-                            ),
-                          );
-                        } else {
-                          return const Icon(
-                            Icons.image_not_supported,
-                            size: 50,
-                          );
-                        }
-                      }),
-                  // QueryArtworkWidget(
-                  //   artworkBorder: BorderRadius.zero,
-                  //   id: item.data![index].id,
-                  //   type: ArtworkType.AUDIO,
-                  // ),
-                  // ignore: avoid_print
-                  onTap: () async {
-                    await audioPlayer.play(
-                      Platform.isAndroid
-                          ? item.data![index].data
-                          : item.data![index].uri!,
-                    );
-
-                    if (fabKey.currentState!.isOpen) {
-                      fabKey.currentState!.close();
-                    } else {
-                      fabKey.currentState!.open();
-                    }
-                  },
-                );
-              },
-            );
-          },
+        body: _widgetList.elementAt(_selectedIndex),
+        bottomNavigationBar: BottomNavigationBar(
+          items: const <BottomNavigationBarItem>[
+            BottomNavigationBarItem(
+                icon: Icon(Icons.home), label: 'audioQuery'),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.photo_album), label: 'FilePicker'),
+          ],
+          currentIndex: _selectedIndex,
+          fixedColor: Colors.blueAccent,
+          onTap: _onItemTapped,
+          type: BottomNavigationBarType.fixed,
         ),
         floatingActionButton: Builder(
           builder: (context) => FabCircularMenu(
@@ -207,7 +147,7 @@ class _SongsState extends State<Songs> {
             onDisplayChange: (isOpen) {
 //              _showSnackBar(
 //                  context, "The menu is ${isOpen ? "open" : "closed"}");
-              print(isOpen ? 'open' : 'closed');
+              debugPrint(isOpen ? 'open' : 'closed');
               setState(() {
                 _visible = !isOpen;
               });
@@ -253,6 +193,95 @@ class _SongsState extends State<Songs> {
           ),
         ),
       ),
+    );
+  }
+
+  Widget _audioQueryBody(BuildContext context) {
+    return FutureBuilder<List<SongModel>>(
+      future: OnAudioQuery().querySongs(
+        sortType: SongSortType.DEFAULT,
+        orderType: OrderType.ASC_OR_SMALLER,
+        uriType: UriType.EXTERNAL,
+      ),
+      builder: (context, item) {
+        // Loading content
+        if (item.data == null) {
+          return const CircularProgressIndicator();
+        }
+
+        // When you try "query"
+        // without asking for [READ] or [Library] permission
+        // the plugin will return a [Empty] list.
+        if (item.data!.isEmpty) {
+          return const Text('Nothing found!');
+        }
+
+        // You can use [item.data!] direct or you can create a:
+        // List<SongModel> songs = item.data!;
+        return ListView.builder(
+          itemCount: item.data!.length,
+          itemBuilder: (context, index) {
+            return ListTile(
+              title: Text(item.data![index].title),
+              subtitle: Text(item.data![index].artist ?? 'No Artist'),
+              // trailing: const Icon(Icons.play_arrow),
+              // This Widget will query/load image. Just add the id and type.
+              // You can use/create your own widget/method using [queryArtwork].
+              leading: FutureBuilder<Uint8List?>(
+                  future: OnAudioQuery().queryArtwork(
+                    item.data![index].id,
+                    ArtworkType.AUDIO,
+                    format: ArtworkFormat.JPEG,
+                    size: 200,
+                  ),
+                  builder: (context, item) {
+                    if (item.data != null && item.data!.isNotEmpty) {
+                      return ClipRRect(
+                        // borderRadius: BorderRadius.circular(50),
+                        clipBehavior: Clip.antiAlias,
+                        child: Image.memory(
+                          item.data!,
+                          gaplessPlayback: false,
+                          repeat: ImageRepeat.noRepeat,
+                          scale: 1,
+                          width: 50,
+                          height: 50,
+                          fit: BoxFit.cover,
+                          // color: artworkColor,
+                          // colorBlendMode: artworkBlendMode,
+                          filterQuality: FilterQuality.low,
+                        ),
+                      );
+                    } else {
+                      return const Icon(
+                        Icons.image_not_supported,
+                        size: 50,
+                      );
+                    }
+                  }),
+              // QueryArtworkWidget(
+              //   artworkBorder: BorderRadius.zero,
+              //   id: item.data![index].id,
+              //   type: ArtworkType.AUDIO,
+              // ),
+              // ignore: avoid_print
+              onTap: () async {
+                await audioPlayer.play(
+                  Platform.isAndroid
+                      ? item.data![index].data
+                      : item.data![index].uri!,
+                );
+
+                if (fabKey.currentState!.isOpen) {
+                  fabKey.currentState!.close();
+                } else {
+                  fabKey.currentState!.open();
+                }
+              },
+            );
+          },
+        );
+      },
     );
   }
 }
